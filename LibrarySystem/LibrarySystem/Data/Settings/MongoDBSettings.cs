2bb36147bfac5cb3d182b02324f2a65904dc02c9
Alex Georgiev <alex_georgiev_03@abv.bg>
2026-02-10 19:54:00 +0200

    Add MongoDB settings

---
+++ LibrarySystem/LibrarySystem/Data/Settings/MongoDBSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace LibrarySystem.Data.Settings
+{
+    public class MongoDBSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string BooksCollection { get; set; } = null!;
+
+        public string MembersCollection { get; set; } = null!;
+    }
+}
