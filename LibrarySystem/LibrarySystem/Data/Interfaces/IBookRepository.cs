be44cb7b9a9cec901c03c7d1a8c856e0feac1cb0
Alex Georgiev <alex_georgiev_03@abv.bg>
2026-02-10 19:16:49 +0200

    Add repository interfaces

---
+++ LibrarySystem/LibrarySystem/Data/Interfaces/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using LibrarySystem.Models.Entities;
+
+namespace LibrarySystem.Data.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task Add(Book book);
+
+        Task<Book?> GetById(string id);
+
+        Task<List<Book>> GetAll();
+
+        Task Update(Book book);
+
+        Task Delete(string id);
+    }
+}
---
+++ LibrarySystem/LibrarySystem/Data/Interfaces/IMemberRepository.cs
@@ -0,0 +1,13 @@
+﻿using LibrarySystem.Models.Entities;
+
+namespace LibrarySystem.Data.Interfaces
+{
+    public interface IMemberRepository
+    {
+        Task Add(Member member);
+
+        Task<Member?> GetById(string id);
+
+        Task<List<Member>> GetAll();
+    }
+}
