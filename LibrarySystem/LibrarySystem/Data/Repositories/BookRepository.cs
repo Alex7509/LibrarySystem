e17847de4930a41371bdac38a3e6419bba42e475
Alex Georgiev <alex_georgiev_03@abv.bg>
2026-02-10 20:20:50 +0200

    Add mongo Db repositories

---
+++ LibrarySystem/LibrarySystem/Data/Repositories/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using LibrarySystem.Data.Interfaces;
+using LibrarySystem.Data.Settings;
+using LibrarySystem.Models.Entities;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace LibrarySystem.Data.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly IMongoCollection<Book> _collection;
+
+        public BookRepository(IOptionsMonitor<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.CurrentValue.ConnectionString);
+
+            var database = client.GetDatabase(settings.CurrentValue.DatabaseName);
+
+            _collection = database.GetCollection<Book>(
+                settings.CurrentValue.BooksCollection);
+        }
+
+        public async Task Add(Book book)
+            => await _collection.InsertOneAsync(book);
+
+        public async Task<Book?> GetById(string id)
+            => await _collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task<List<Book>> GetAll()
+            => await _collection.Find(_ => true).ToListAsync();
+
+        public async Task Update(Book book)
+            => await _collection.ReplaceOneAsync(x => x.Id == book.Id, book);
+
+        public async Task Delete(string id)
+            => await _collection.DeleteOneAsync(x => x.Id == id);
+    }
+}
---
+++ LibrarySystem/LibrarySystem/Data/Repositories/MemberRepository.cs
@@ -0,0 +1,33 @@
+﻿using LibrarySystem.Data.Interfaces;
+using LibrarySystem.Data.Settings;
+using LibrarySystem.Models.Entities;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace LibrarySystem.Data.Repositories
+{
+    public class MemberRepository : IMemberRepository
+    {
+        private readonly IMongoCollection<Member> _collection;
+
+        public MemberRepository(IOptionsMonitor<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.CurrentValue.ConnectionString);
+
+            var database = client.GetDatabase(settings.CurrentValue.DatabaseName);
+
+            _collection = database.GetCollection<Member>(
+                settings.CurrentValue.MembersCollection);
+        }
+
+        public async Task Add(Member member)
+            => await _collection.InsertOneAsync(member);
+
+        public async Task<Member?> GetById(string id)
+            => await _collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task<List<Member>> GetAll()
+            => await _collection.Find(_ => true).ToListAsync();
+
+    }
+}
