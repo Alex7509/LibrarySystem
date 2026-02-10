7c6e10ce6bd9baaf0640d099d267f00ca0da6f3b
Alex Georgiev <alex_georgiev_03@abv.bg>
2026-02-10 18:47:30 +0200

    Add request models

---
+++ LibrarySystem/LibrarySystem/Models/Requests/AddBookRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace LibrarySystem.Models.Requests
+{
+    public class AddBookRequest
+    {
+        public string Title { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+
+        public int Year { get; set; }
+    }
+}
---
+++ LibrarySystem/LibrarySystem/Models/Requests/AddMemberRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace LibrarySystem.Models.Requests
+{
+    public class AddMemberRequest
+    {
+        public string Name { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+    }
+}
