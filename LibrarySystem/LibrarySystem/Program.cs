a103896733f092db939408d53581848bfc69a6da
Alex Georgiev <alex_georgiev_03@abv.bg>
2026-02-12 11:28:20 +0200

    Configure dependency injection and Swagger

--- LibrarySystem/LibrarySystem/Program.cs
+++ LibrarySystem/LibrarySystem/Program.cs
@@ -1,25 +1,32 @@
+using LibrarySystem.Business.Interfaces;
+using LibrarySystem.Business.Services;
+using LibrarySystem.Data.Interfaces;
+using LibrarySystem.Data.Repositories;
+using LibrarySystem.Data.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<IMemberService, MemberService>();
+builder.Services.AddScoped<IBorrowService, BorrowService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
 app.Run();
+
