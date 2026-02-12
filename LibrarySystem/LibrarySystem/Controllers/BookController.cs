6d07eff846df9d12fc8286954075b8caf6d8b651
Alex Georgiev <alex_georgiev_03@abv.bg>
2026-02-12 11:18:03 +0200

    Add API Controllers

---
+++ LibrarySystem/LibrarySystem/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using LibrarySystem.Business.Interfaces;
+using LibrarySystem.Models.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.Controllers;
+
+[ApiController]
+[Route("api/books")]
+public class BookController : ControllerBase
+{
+    private readonly IBookService _bookService;
+
+    public BookController(IBookService bookService)
+    {
+        _bookService = bookService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddBook(AddBookRequest request)
+    {
+        var response = await _bookService.AddBook(request);
+
+        return Ok(response);
+    }
+}
+
+
---
+++ LibrarySystem/LibrarySystem/Controllers/BorrowController.cs
@@ -0,0 +1,32 @@
+﻿using LibrarySystem.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.Controllers;
+
+[ApiController]
+[Route("api/borrow")]
+public class BorrowController : ControllerBase
+{
+    private readonly IBorrowService _borrowService;
+
+    public BorrowController(IBorrowService borrowService)
+    {
+        _borrowService = borrowService;
+    }
+
+    [HttpPost("{bookId}/{memberId}")]
+    public async Task<IActionResult> Borrow(string bookId, string memberId)
+    {
+        await _borrowService.BorrowBook(bookId, memberId);
+
+        return Ok();
+    }
+
+    [HttpPost("return/{bookId}")]
+    public async Task<IActionResult> Return(string bookId)
+    {
+        await _borrowService.ReturnBook(bookId);
+
+        return Ok();
+    }
+}
