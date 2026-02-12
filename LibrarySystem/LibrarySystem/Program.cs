34eb8056efb5ea73899c42e85e650f3e4946853f
Alex Georgiev <alex_georgiev_03@abv.bg>
2026-02-12 12:03:16 +0200

    Add Serilog logging and HealthCheck endpoint

--- LibrarySystem/LibrarySystem/Program.cs
+++ LibrarySystem/LibrarySystem/Program.cs
@@ -1,3 +1,4 @@
+using Serilog;
 using LibrarySystem.Business.Interfaces;
 using LibrarySystem.Business.Services;
 using LibrarySystem.Data.Interfaces;
@@ -6,9 +7,19 @@ using LibrarySystem.Data.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Serilog configuration
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// MongoDB settings
 builder.Services.Configure<MongoDbSettings>(
     builder.Configuration.GetSection("MongoDbSettings"));
 
+// Dependency Injection
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 
@@ -17,9 +28,11 @@ builder.Services.AddScoped<IMemberService, MemberService>();
 builder.Services.AddScoped<IBorrowService, BorrowService>();
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// HealthCheck
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -27,6 +40,6 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
-
